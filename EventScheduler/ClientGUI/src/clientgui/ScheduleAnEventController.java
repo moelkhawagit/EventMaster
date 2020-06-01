06cd23b741a190490b39d60da2dcba5578dd7036
Ahmed Saad <ahmedsaad17391739@gmail.com>
2020-06-01 16:10:19 -0400

    Added Functionality in Schedule and event

--- EventScheduler/ClientGUI/src/clientgui/ScheduleAnEventController.java
+++ EventScheduler/ClientGUI/src/clientgui/ScheduleAnEventController.java
@@ -9,7 +9,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 /**
@@ -75,6 +80,14 @@ public class ScheduleAnEventController implements Initializable {
             newEventManager.createEvent(EventTitle,localDate.toString(),RoomId,transfarevalue);
         }
 
+    }
+    
+    public void back(ActionEvent event) throws IOException{
+        Parent root= FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+        Scene receivingScene = new Scene(root);
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(receivingScene);
+        window.show();  
     }
     
         
